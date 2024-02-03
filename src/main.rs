d5bc0546dccf57b08807fd27bfb8f9ada12f5f1b
Felipe Mattes <mattesfelipe@gmail.com>
2024-02-03 18:45:27 -0300

    created connection with diesel to make migrations

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
+
+extern crate diesel;
+
 use actix_web::{web, App, HttpServer};
+use diesel::pg::PgConnection;
+use diesel::r2d2::ConnectionManager;
+
 
 mod handlers;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    //Para ler .env
+    dotenv::dotenv().ok();
+
     std::env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Falha ao criar a pool");
 
     // Start http server
     HttpServer::new(move || {
         App::new()
+            .app_data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
