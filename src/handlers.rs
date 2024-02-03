2fbf9bfc2043a3f2136572340fa23c61456e8f70
Felipe Mattes <mattesfelipe@gmail.com>
2024-02-03 19:07:30 -0300

    migration structure

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,6 @@
 use actix_web::Responder;
 
+
 pub async fn get_users() -> impl Responder {
     format!("hello from get users")
 }
---
+++ src/models.rs
@@ -0,0 +1,11 @@
+use crate::schema::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+pub struct User {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub created_at: chrono::NaiveDateTime,
+}
